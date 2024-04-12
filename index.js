773033046624ea24b5938568b94da7cc8f8a9845
Louie <loumpach@hotmail.com>
2024-04-12 13:25:11 +0800

    Updated message

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World! Hello Louie");
+    return res.send("Hello World! Hello Louie P, Happy Weekend!");
 })
 
 app.get('/api', function (req, res) {
